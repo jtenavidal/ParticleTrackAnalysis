130c8d403454d8f64467ba90e0b707b67d526d10
jtenavidal <j.tena-vidal@liverpool.ac.uk>
2019-02-06 17:50:54 +0000

    adding info from root file. event_v/econtained breaks

--- TrackFitter/TrackFitter.cpp
+++ TrackFitter/TrackFitter.cpp
@@ -34,6 +34,10 @@ TrackFitter::TrackFitter( const Track & p_track ){
    // Event Tree
    TTree * event_tree   = (TTree*) track_file.Get("event_tree") ;
    TBranch * event_id = event_tree->GetBranch("event_id");
+   TBranch * is_reconstructed = event_tree->GetBranch("is_reconstructed");
+   TBranch * has_reco_daughters = event_tree->GetBranch("has_reco_daughters");
+   TBranch * has_reco_tracks = event_tree->GetBranch("has_reco_tracks");
+   TBranch * has_reco_showers = event_tree->GetBranch("has_reco_showers");
    // MC Tree
    TTree * mcparticle_tree   = (TTree*) track_file.Get("mcparticle_tree") ;
    TBranch * mc_event_id = mcparticle_tree->GetBranch("event_id");
@@ -58,6 +62,8 @@ TrackFitter::TrackFitter( const Track & p_track ){
    // RECO Tree
    TTree * recoparticle_tree   = (TTree*) track_file.Get("recoparticle_tree") ;
    TBranch * reco_event_id = recoparticle_tree->GetBranch("event_id");
+   TBranch * primary_vcontained = recoparticle_tree->GetBranch("primary_vcontained");
+   TBranch * primary_econtained = recoparticle_tree->GetBranch("primary_econtained");
    TBranch * r_pdg_primary = recoparticle_tree->GetBranch("r_pdg_primary");
    TBranch * nu_daughters = recoparticle_tree->GetBranch("r_nu_daughters");
    TBranch * Length = recoparticle_tree->GetBranch("rLength");
@@ -73,9 +79,64 @@ TrackFitter::TrackFitter( const Track & p_track ){
    TBranch * r_track_z = recoparticle_tree->GetBranch("r_track_z");
    TBranch * r_track_dEdx = recoparticle_tree->GetBranch("r_track_dEdx");
    TBranch * r_dEdx = recoparticle_tree->GetBranch("r_dEdx");
+   TBranch * pfps_hits = recoparticle_tree->GetBranch("pfps_hits");
+   TBranch * pfps_type = recoparticle_tree->GetBranch("pfps_type");
+   TBranch * event_vcontained = recoparticle_tree->GetBranch("event_vcontained");
+   TBranch * event_econtained = recoparticle_tree->GetBranch("event_econtained");
+   TBranch * pfps_length = recoparticle_tree->GetBranch("pfps_length");
+   TBranch * pfps_dir_start_x = recoparticle_tree->GetBranch("pfps_dir_start_x");
+   TBranch * pfps_dir_start_y = recoparticle_tree->GetBranch("pfps_dir_start_y");
+   TBranch * pfps_dir_start_z = recoparticle_tree->GetBranch("pfps_dir_start_z");
+   TBranch * pfps_dir_end_x = recoparticle_tree->GetBranch("pfps_dir_end_x");
+   TBranch * pfps_dir_end_y = recoparticle_tree->GetBranch("pfps_dir_end_y");
+   TBranch * pfps_dir_end_z = recoparticle_tree->GetBranch("pfps_dir_end_z");
+   TBranch * pfps_start_x = recoparticle_tree->GetBranch("pfps_start_x");
+   TBranch * pfps_start_y = recoparticle_tree->GetBranch("pfps_start_y");
+   TBranch * pfps_start_z = recoparticle_tree->GetBranch("pfps_start_z");
+   TBranch * pfps_end_x = recoparticle_tree->GetBranch("pfps_end_x");
+   TBranch * pfps_end_y = recoparticle_tree->GetBranch("pfps_end_y");
+   TBranch * pfps_end_z = recoparticle_tree->GetBranch("pfps_end_z");
 
+
+   /**
+     * General event information
+     */
+     for( unsigned int i = 0; i < event_tree->GetEntries(); ++i ){
+       event_tree->GetEntry(i);
+       _is_reconstructed.push_back(is_reconstructed->GetLeaf("is_reconstructed")->GetValue());
+       _has_reco_daughters.push_back(has_reco_daughters->GetLeaf("has_reco_daughters")->GetValue());
+       _has_reco_tracks.push_back(has_reco_tracks->GetLeaf("has_reco_tracks")->GetValue());
+       _has_reco_showers.push_back(has_reco_showers->GetLeaf("has_reco_showers")->GetValue());
+      }
+
+   /**
+     * MC information
+     */
+    for( unsigned int i = 0; i < mcparticle_tree->GetEntries(); ++i ){
+      mcparticle_tree->GetEntry(i);
+      //_event_TLenght.push_back( MC_Lenght->GetLeaf("fMCLength")->GetValue() ) ; // still breaks
+      _Tnu_daughters.push_back(Num_Daughters->GetLeaf("fNumDaughters")->GetValue());
+      _TPDG_Code_Primary.push_back( PDG_Code->GetLeaf("fPDG_Code")->GetValue() ) ;
+      _Tnu_mu.push_back( Daughter_mu->GetLeaf("fDaughter_mu")->GetValue() ) ;
+      _Tnu_pi.push_back( Daughter_pi->GetLeaf("fDaughter_pi")->GetValue() ) ;
+      _Tnu_p.push_back( Daughter_p->GetLeaf("fDaughter_p")->GetValue() ) ;
+      _Tnu_e.push_back( Daughter_e->GetLeaf("fDaughter_e")->GetValue() ) ;
+      _Tnu_n.push_back( Daughter_n->GetLeaf("fDaughter_n")->GetValue() );
+      _Tnu_photon.push_back( Daughter_photon->GetLeaf("fDaughter_photon")->GetValue() );
+      _Tnu_others.push_back( Daughter_other->GetLeaf("fDaughter_other")->GetValue() );
+     }
+
+/**
+  * Reco information
+  */
    Hit_level track_hit_x, track_hit_y, track_hit_z, track_hit_dEdx;
    std::vector< double > chi2_mu, chi2_pi, chi2_p, pida ;
+   std::vector< bool > vcontained, econtained ;
+   std::vector< int > pfps_hits_i, pfps_type_i ;
+   std::vector< float > pfps_length_i ;
+   std::vector< double > pfps_dir_start_i_x, pfps_dir_start_i_y, pfps_dir_start_i_z, pfps_dir_end_i_x, pfps_dir_end_i_y, pfps_dir_end_i_z ;
+   std::vector< double > pfps_start_i_x, pfps_start_i_y, pfps_start_i_z, pfps_end_i_x, pfps_end_i_y, pfps_end_i_z ;
+
 
    for( unsigned int i = 0; i < recoparticle_tree->GetEntries(); ++i ){
      _particle_track.clear();
@@ -84,10 +145,34 @@ TrackFitter::TrackFitter( const Track & p_track ){
      track_hit_z.clear();
      track_hit_dEdx.clear();
      _reco_dEdx.clear();
+     chi2_mu.clear();
+     chi2_pi.clear();
+     chi2_p.clear();
+     pida.clear();
+     vcontained.clear() ;
+     econtained.clear() ;
+     pfps_hits_i.clear() ;
+     pfps_type_i.clear() ;
+     pfps_length_i.clear() ;
+     pfps_dir_start_i_x.clear() ;
+     pfps_dir_start_i_y.clear() ;
+     pfps_dir_start_i_z.clear() ;
+     pfps_dir_end_i_x.clear() ;
+     pfps_dir_end_i_y.clear() ;
+     pfps_dir_end_i_z.clear() ;
+     pfps_start_i_x.clear() ;
+     pfps_start_i_y.clear() ;
+     pfps_start_i_z.clear() ;
+     pfps_end_i_x.clear() ;
+     pfps_end_i_y.clear() ;
+     pfps_end_i_z.clear() ;
+
 
      recoparticle_tree->GetEntry(i);
      _hits = rnu_hits->GetLeaf("rnu_hits")->GetValue() ;
      _event_hits.push_back( _hits ) ;
+     _event_primamry_vcontained.push_back( primary_vcontained->GetLeaf("primary_vcontained")->GetValue() ) ;
+     _event_primary_econtained.push_back( primary_econtained->GetLeaf("primary_econtained")->GetValue() ) ;
 
      if ( _hits != 0 ) {
        _event_RLenght.push_back( Length->GetLeaf("rLength")->GetValue() );
@@ -112,11 +197,45 @@ TrackFitter::TrackFitter( const Track & p_track ){
          chi2_pi.push_back( r_chi2_pi->GetLeaf("r_chi2_pi")->GetValue(j));
          chi2_p.push_back ( r_chi2_p ->GetLeaf("r_chi2_p")->GetValue(j));
          pida.push_back   ( r_PIDA   ->GetLeaf("r_PIDA")->GetValue(j));
+         //vcontained.push_back( event_vcontained->GetLeaf("event_vcontained")->GetValue(j) ) ;
+         //econtained.push_back( event_vcontained->GetLeaf("event_econtained")->GetValue(j) ) ;
+         pfps_hits_i.push_back( pfps_hits->GetLeaf("pfps_hits")->GetValue(j) ) ;
+         pfps_type_i.push_back( pfps_type->GetLeaf("pfps_type")->GetValue(j) ) ;
+         pfps_length_i.push_back( pfps_length->GetLeaf("pfps_length")->GetValue(j) ) ;
+         pfps_dir_start_i_x.push_back( pfps_dir_start_x->GetLeaf("pfps_dir_start_x")->GetValue(j) ) ;
+         pfps_dir_start_i_y.push_back( pfps_dir_start_y->GetLeaf("pfps_dir_start_y")->GetValue(j) ) ;
+         pfps_dir_start_i_z.push_back( pfps_dir_start_z->GetLeaf("pfps_dir_start_z")->GetValue(j) ) ;
+         pfps_dir_end_i_x.push_back( pfps_dir_end_x->GetLeaf("pfps_dir_end_x")->GetValue(j) ) ;
+         pfps_dir_end_i_y.push_back( pfps_dir_end_y->GetLeaf("pfps_dir_end_y")->GetValue(j) ) ;
+         pfps_dir_end_i_z.push_back( pfps_dir_end_z->GetLeaf("pfps_dir_end_z")->GetValue(j) ) ;
+         pfps_start_i_x.push_back( pfps_start_x->GetLeaf("pfps_start_x")->GetValue(j) ) ;
+         pfps_start_i_y.push_back( pfps_start_y->GetLeaf("pfps_start_y")->GetValue(j) ) ;
+         pfps_start_i_z.push_back( pfps_start_z->GetLeaf("pfps_start_z")->GetValue(j) ) ;
+         pfps_end_i_x.push_back( pfps_end_x->GetLeaf("pfps_end_x")->GetValue(j) ) ;
+         pfps_end_i_y.push_back( pfps_end_y->GetLeaf("pfps_end_y")->GetValue(j) ) ;
+         pfps_end_i_z.push_back( pfps_end_z->GetLeaf("pfps_end_z")->GetValue(j) ) ;
        }
        _event_chi2_mu.push_back(chi2_mu);
        _event_chi2_pi.push_back(chi2_pi);
        _event_chi2_p.push_back(chi2_p);
        _event_PIDA.push_back(pida);
+       _event_pfps_hits.push_back(pfps_hits_i);
+       _event_pfps_type.push_back(pfps_type_i);
+       //_event_pfps_vcontained.push_back(vcontained);
+       //_event_pfps_econtained.push_back(econtained);
+       _event_pfps_length.push_back(pfps_length_i);
+       _event_pfps_dir_start_x.push_back(pfps_dir_start_i_x);
+       _event_pfps_dir_start_y.push_back(pfps_dir_start_i_y);
+       _event_pfps_dir_start_z.push_back(pfps_dir_start_i_z);
+       _event_pfps_dir_end_x.push_back(pfps_dir_end_i_x);
+       _event_pfps_dir_end_y.push_back(pfps_dir_end_i_y);
+       _event_pfps_dir_end_z.push_back(pfps_dir_end_i_z);
+       _event_pfps_start_x.push_back(pfps_start_i_x);
+       _event_pfps_start_y.push_back(pfps_start_i_y);
+       _event_pfps_start_z.push_back(pfps_start_i_z);
+       _event_pfps_end_x.push_back(pfps_end_i_x);
+       _event_pfps_end_y.push_back(pfps_end_i_y);
+       _event_pfps_end_z.push_back(pfps_end_i_z);
 
    } else {
      // seting to zero for empty events
@@ -152,20 +271,6 @@ TrackFitter::TrackFitter( const Track & p_track ){
  _Tnu_photon.clear();
  _Tnu_others.clear();
  _Tnu_daughters.clear();
-
- for( unsigned int i = 0; i < mcparticle_tree->GetEntries(); ++i ){
-   mcparticle_tree->GetEntry(i);
-   //_event_TLenght.push_back( MC_Lenght->GetLeaf("fMCLength")->GetValue() ) ; // still breaks
-   _Tnu_daughters.push_back(Num_Daughters->GetLeaf("fNumDaughters")->GetValue());
-   _TPDG_Code_Primary.push_back( PDG_Code->GetLeaf("fPDG_Code")->GetValue() ) ;
-   _Tnu_mu.push_back( Daughter_mu->GetLeaf("fDaughter_mu")->GetValue() ) ;
-   _Tnu_pi.push_back( Daughter_pi->GetLeaf("fDaughter_pi")->GetValue() ) ;
-   _Tnu_p.push_back( Daughter_p->GetLeaf("fDaughter_p")->GetValue() ) ;
-   _Tnu_e.push_back( Daughter_e->GetLeaf("fDaughter_e")->GetValue() ) ;
-   _Tnu_n.push_back( Daughter_n->GetLeaf("fDaughter_n")->GetValue() );
-   _Tnu_photon.push_back( Daughter_photon->GetLeaf("fDaughter_photon")->GetValue() );
-   _Tnu_others.push_back( Daughter_other->GetLeaf("fDaughter_other")->GetValue() );
-  }
 
 }
 
@@ -212,7 +317,7 @@ void TrackFitter::TruthParticles( unsigned int & event_id_track ){
 void TrackFitter::SaveTrack( std::string const & path , const unsigned int & event_id_track ) {
   std::vector< std::vector< double > > min_Linearity_position = FindMinimumLinearityPosition( event_id_track ) ;
   int bins = int(_event_hits[event_id_track-1]/10) ;
-  int window =  int( _event_hits[event_id_track-1] * 0.05 ) ;
+  int window =  int( _event_hits[event_id_track-1] * 0.07 ) ;
   if( window < 5 ) window = 5 ;
 
   TH3D *h_track = new TH3D("h_track", " Particle Track ", bins,
@@ -231,7 +336,7 @@ _event_tracks[event_id_track-1][2][0], _event_tracks[event_id_track-1][2][_event
   TLegend *leg = new TLegend(0.9,0.7,0.48,0.9);
 
   for( int i = 0; i < _event_hits[event_id_track-1]; ++i ){
-    h_track-> Fill(_event_tracks[event_id_track-1][0][i], _event_tracks[event_id_track-1][1][i], _event_tracks[event_id_track-1][2][i] ) ; } //, _event_tracks[event_id_track-1][3][i]);  }
+    h_track-> Fill(_event_tracks[event_id_track-1][0][i], _event_tracks[event_id_track-1][1][i], _event_tracks[event_id_track-1][2][i] ) ; }//, _event_tracks[event_id_track-1][3][i]);  }
 
   TCanvas *c = new TCanvas();
   gStyle->SetPalette(55);
@@ -278,7 +383,7 @@ void TrackFitter::PlotLinearityData( const std::string & path , const unsigned i
   TH1F * h_Linearity = new TH1F( "h_Linearity", "Linearity", corrP.size(), 0,  corrP.size() );
   TLegend * legend = new TLegend(0.15,0.15,0.35,0.35) ;
 
-  int window =  int( _event_hits[event_id_track-1] * 0.05 ) ;
+  int window =  int( _event_hits[event_id_track-1] * 0.07 ) ;
   if( window < 5 ) window = 5 ;
 
   gStyle->SetOptStat(0);
@@ -304,7 +409,7 @@ void TrackFitter::PlotLinearityTrack( const std::string & path , const unsigned 
   std::vector< double > corrP_XY = LinearityData( _event_tracks[event_id_track-1][0], _event_tracks[event_id_track-1][1] ) ;
   std::vector< double > corrP_XZ = LinearityData( _event_tracks[event_id_track-1][0], _event_tracks[event_id_track-1][2] ) ;
   std::vector< double > corrP_YZ = LinearityData( _event_tracks[event_id_track-1][2], _event_tracks[event_id_track-1][1] ) ;
-  int window =  int( _event_hits[event_id_track-1] * 0.05 ) ;
+  int window =  int( _event_hits[event_id_track-1] * 0.07 ) ;
   if( window < 5 ) window = 5 ;
 
   TH1F * h_Linearity = new TH1F( "h_Linearity", "Linearity", corrP_XY.size(), 0,  corrP_XY.size() );
@@ -349,6 +454,8 @@ void TrackFitter::PlotLinearityTrack( const std::string & path , const unsigned 
   h_LinearityXY -> Draw("HIST L SAME") ;
   h_LinearityXZ -> Draw("HIST L SAME") ;
   h_LinearityYZ -> Draw("HIST L SAME") ;
+  h_Linearity->GetXaxis()->SetTitle("hits");
+  h_Linearity->GetYaxis()->SetTitle("r");
   legend->Draw();
   c->SaveAs( (path+"_LinearityX.root").c_str() ) ;
 
@@ -482,7 +589,7 @@ std::vector< TVector3 > TrackFitter::MeanDirectionData( const unsigned int & eve
   unsigned int starting_hit , end_hit;
   TVector3 directionI ;
   std::vector< TVector3 > mean_direction ;
-  int window =  int( _event_hits[event_id_track-1] * 0.05 ) ;
+  int window =  int( _event_hits[event_id_track-1] * 0.07 ) ;
   if( window < 5 ) window = 5 ;
 
   for( int i = 0; i < int(_event_hits[event_id_track-1]); ++i ){
@@ -533,7 +640,7 @@ std::vector< std::vector< double > > TrackFitter::FindMinimumLinearityPosition( 
     std::vector< double > corrP_XZ = LinearityData( _event_tracks[event_id_track-1][0], _event_tracks[event_id_track-1][2] ) ;
     std::vector< double > corrP_YZ = LinearityData( _event_tracks[event_id_track-1][2], _event_tracks[event_id_track-1][1] ) ;
     std::vector< double > corrP ;
-    int window =  int( _event_hits[event_id_track-1] * 0.05 ) ;
+    int window =  int( _event_hits[event_id_track-1] * 0.07 ) ;
     if( window < 5 ) window = 5 ;
     double linearity_min = 2 ;
     unsigned int min_hit = 0 ;
@@ -554,22 +661,30 @@ std::vector< std::vector< double > > TrackFitter::FindMinimumLinearityPosition( 
           position.push_back( _event_tracks[event_id_track-1][1][min_hit] ) ;
           position.push_back( _event_tracks[event_id_track-1][2][min_hit] ) ;
           min_Linearity_position.push_back( position ) ;
+          // std::cout<< " x hit  = " << _event_tracks[event_id_track-1][0][min_hit] << " y hit = " << _event_tracks[event_id_track-1][1][min_hit] << " z hit = " << _event_tracks[event_id_track-1][2][min_hit] << std::endl;
+
           position.clear();
           linearity_min = 2 ;
         }
     }
+
     return min_Linearity_position;
 }
 
-void TrackFitter::StatisticsKinks( const unsigned int & event_id_track ){
+void TrackFitter::StatisticsKinks( ){
   int has_kink = 0 , is_straight = 0 , has_one_kink = 0 , has_more_kinks = 0 ;
   for ( unsigned int i = 1 ; i < _event_tracks.size() +1 ; ++i ){
     if ( FindMinimumLinearityPosition( i ).size() > 0 ) {
       ++has_kink ;
-      if ( FindMinimumLinearityPosition( i ).size() == 1 ) ++has_one_kink ;
+      if ( FindMinimumLinearityPosition( i ).size() == 1 ) {
+          ++has_one_kink ;
+        }
       if ( FindMinimumLinearityPosition( i ).size() > 1 ) {
           ++has_more_kinks ;
-          std::cout<< " Event ID = " << i << std::endl ;
+          std::cout<< " Event ID = " << i << std::endl;
+          SaveTrack( ("Track_testing_new_constructor_"+std::to_string(i)) , i ) ;
+          unsigned int event = i ;
+          PlotLinearityTrack( " testing "+std::to_string(i) , event );
         }
     }
     else { ++is_straight ; }
@@ -577,6 +692,9 @@ void TrackFitter::StatisticsKinks( const unsigned int & event_id_track ){
 
   std::cout<< " % kinked tracks = " << has_kink*100/(has_kink+is_straight) << std::endl;
   std::cout<< " --->   % 1  kinked tracks = " << has_one_kink*100/has_kink << std::endl;
+  std::cout<< " kinq 1 " << has_one_kink << std::endl;
+
+  std::cout<< " kinq 1 " << has_more_kinks << std::endl;
   std::cout<< " --->   % >1 kinked tracks = " << has_more_kinks*100/has_kink << std::endl;
   std::cout<< " % straight tracks = " << is_straight*100/(has_kink+is_straight) << std::endl;
 
--- TrackFitter/TrackFitter.h
+++ TrackFitter/TrackFitter.h
@@ -49,15 +49,19 @@ typedef std::vector< double > Hit_level;
     //double FitToLine( ) ;
     std::vector< TVector3 > MeanDirectionData( const unsigned int & event_id_track) ;
     std::vector< double > AngleTrackDistribution( const unsigned int & event_id_track) ;
-    void StatisticsKinks( const unsigned int & event_id_track ) ;
+    void StatisticsKinks( ) ;
 
   private :
+  // Event tree information
+  std::vector< bool > _is_reconstructed, _has_reco_daughters, _has_reco_tracks, _has_reco_showers ;
+
   // Object Truth information
   std::vector< double > _event_TLenght ;
   std::vector< int > _Tnu_daughters, _TPDG_Code_Primary, _Tnu_mu, _Tnu_pi, _Tnu_p, _Tnu_e, _Tnu_n, _Tnu_photon, _Tnu_others ; // truth information pdg hiearchy
 
   // Object RECO information
   int _hits ;
+  std::vector< bool > _event_primamry_vcontained, _event_primary_econtained ;
   std::vector< double > _event_RLenght ;
   Hit_level _vertex_position, _end_position ;
   std::vector< float > _reco_dEdx ;
@@ -67,6 +71,12 @@ typedef std::vector< double > Hit_level;
   std::vector< std::vector<double> > _event_vertex, _event_end, _event_chi2_mu, _event_chi2_pi, _event_chi2_p, _event_PIDA  ;
   std::vector< std::vector< float > > _event_reco_dEdx ;
   std::vector< Track > _event_tracks;  // maps event and track
+  // Keeping hiearchy information per event_tree
+  std::vector< std::vector< int > > _event_pfps_hits, _event_pfps_type ;
+  std::vector< std::vector< bool > > _event_pfps_vcontained, _event_pfps_econtained ;
+  std::vector< std::vector< float > > _event_pfps_length ;
+  std::vector< std::vector< double > > _event_pfps_dir_start_x, _event_pfps_dir_start_y, _event_pfps_dir_start_z, _event_pfps_dir_end_x,  _event_pfps_dir_end_y, _event_pfps_dir_end_z ;
+  std::vector< std::vector< double > > _event_pfps_start_x, _event_pfps_start_y, _event_pfps_start_z, _event_pfps_end_x,  _event_pfps_end_y, _event_pfps_end_z ;
 
   /**
   * Functions to guess about the geometry of the track
@@ -82,6 +92,8 @@ typedef std::vector< double > Hit_level;
   std::vector< double > LinearityData( const std::vector< double > & Data_1, const std::vector< double > & Data_2 ) ;
   std::vector< std::vector< double > > FindMinimumLinearityPosition( const unsigned int & event_id_track );
 
+
+  // Need to add ClearTemporary, ClearAll, Delete functions
   }; // Event
 
 
