88a057e6ace3ad305e15806f19d4c1b47b8b6e23
Julia Tena Vidal <jtenavid@sbndgpvm01.fnal.gov>
2019-05-14 04:12:53 -0500

    bash file to obtain full path/name.root to use for the analysis

---
+++ bash_allpaths.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#!/bin/bash
+
+FILE=$1
+while read LINE; do
+    STR=`samweb -e sbnd locate-file $LINE`
+    STR=${STR/(*)}
+    STR=${STR/enstore:}
+    STR="$STR/"
+    STR=$STR$LINE
+    echo $STR >> "complete_path.txt"
+done < $FILE
