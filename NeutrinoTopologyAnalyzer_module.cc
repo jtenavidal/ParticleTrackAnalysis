af914ad6f67bd9db75830d350327ca3ba6d2546e
Julia Tena Vidal <jtenavid@sbndgpvm01.fnal.gov>
2019-05-28 07:24:27 -0500

    fixed bug

--- NeutrinoTopologyAnalyzer_module.cc
+++ NeutrinoTopologyAnalyzer_module.cc
@@ -216,9 +216,9 @@ private:
   THStack * h_MCLength_p_TPC  = new THStack("h_MCLength_p_TPC" , "P MC Length TPC");
 
   // Hiearchy information for a candidate signature
-  TH1D * h_recoDaughters_mu = new TH1D("recoDaughters_mu", " Total number of daughters for muons" , 3, -0.5, 3.5 ) ;
-  TH1D * h_recoDaughters_pi = new TH1D("recoDaughters_pi", " Total number of daughters for pions" , 3, -0.5, 3.5 ) ;
-  TH1D * h_recoDaughters_p = new TH1D("recoDaughters_p", " Total number of daughters for protons" , 3, -0.5, 3.5 ) ;
+  TH1D * h_recoDaughters_mu = new TH1D("recoDaughters_mu", " Total number of daughters for muons" , 4, -0.5, 3.5 ) ;
+  TH1D * h_recoDaughters_pi = new TH1D("recoDaughters_pi", " Total number of daughters for pions" , 4, -0.5, 3.5 ) ;
+  TH1D * h_recoDaughters_p = new TH1D("recoDaughters_p", " Total number of daughters for protons" , 4, -0.5, 3.5 ) ;
 
 
 
@@ -524,14 +524,15 @@ void test::NeutrinoTopologyAnalyzer::analyze(art::Event const& e)
   }
 
   for( it = map_RecoHiearchy.begin(); it != map_RecoHiearchy.end() ; ++it ) {
-    if( it -> first == 1 && mapMC_reco_pdg[ it -> first ] == 13 ){
+    if( it -> second == 1 && mapMC_reco_pdg[ it -> first ] == 13 ){
       if( map_recoDaughters.find( it -> first ) != map_recoDaughters.end() ) { 
 	if( map_recoDaughters[it->first].size() > 2) h_recoDaughters_mu -> Fill( 3 ) ; // 3 means more than 2. 
 	else h_recoDaughters_mu -> Fill( map_recoDaughters[it->first].size() ) ;
       }
       else h_recoDaughters_mu -> Fill( 0 ) ;
     } 
-    if( it -> first == 1 && mapMC_reco_pdg[ it -> first ] == 211 ){
+    if( it -> second == 1 && mapMC_reco_pdg[ it -> first ] == 211 ){
+      std::cout<< " FOUND PRIMARY PION " << std::endl;
       if( map_recoDaughters.find( it -> first ) != map_recoDaughters.end() ) { 
 	if( map_recoDaughters[it->first].size() > 2) h_recoDaughters_pi -> Fill( 3 ) ; // 3 means more than 2. 
 	else h_recoDaughters_pi -> Fill( map_recoDaughters[it->first].size() ) ;
@@ -539,7 +540,7 @@ void test::NeutrinoTopologyAnalyzer::analyze(art::Event const& e)
       else h_recoDaughters_pi -> Fill( 0 ) ;
     } 
 
-    if( it -> first == 1 && mapMC_reco_pdg[ it -> first ] == 2212 ){
+    if( it -> second == 1 && mapMC_reco_pdg[ it -> first ] == 2212 ){
       if( map_recoDaughters.find( it -> first ) != map_recoDaughters.end() ) { 
 	if( map_recoDaughters[it->first].size() > 2) h_recoDaughters_p -> Fill( 3 ) ; // 3 means more than 2. 
 	else h_recoDaughters_p -> Fill( map_recoDaughters[it->first].size() ) ;
